b5c9bbdd58f04dc730b80d0d7da02051f655e1a1
Kalinin Evgeniy <kalinin.gdn@gmail.com>
2025-08-21 10:04:58 +0300

    Именена основная логика

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (number + number2));
+        System.out.println("Разность чисел: " + (number - number2));
+        System.out.println("Произведение чисел: " + (number * number2));
+        double d=number;
+        double d2=number2;
+        System.out.println("Частное чисел: " + (d/d2));
     }
 }
