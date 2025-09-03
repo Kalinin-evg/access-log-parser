0a3aedaeeed38360ed6358eecedc63d7d96c31f3
Kalinin Evgeniy <kalinin.gdn@gmail.com>
2025-09-03 11:22:48 +0300

    добавлена основная логика поиска файла

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,32 @@
 import java.util.Scanner;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        int correctFileCount = 0;
 
+        while (true) {
+            System.out.println("Введите путь к файлу, например: C:\\Users\\username\\Документы\\файл.txt и нажмите <Enter>:");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Файл не найден.Введите корректное имя файла и нажмите <Enter>.");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Указанный путь ведёт к папке, а не к файлу. Попробуйте снова.");
+                continue;
+            } else {
+                correctFileCount++;
+                System.out.println("Путь указан верно");
+                System.out.println("Количество вернных вводов " + correctFileCount);
+            }
+        }
     }
 }
+
